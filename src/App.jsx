867343105b7fcf543c55b7a22b0a5c7b0953c28d
iSaAcLEmMa <yishaklemma878@gmail.com>
2025-11-27 21:58:48 +0300

    my second commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+import Yishak from './yishak'
+
+
+const App = () => {
+  return (
+    <>
+     <Yishak/>
+      
+     
+    </>
+
+      
+    
+  )
+}
+
+export default App
---
+++ src/yishak.jsx
@@ -0,0 +1,24 @@
+    import IMG_3241 from './assets/IMG_3241.jpeg'
+    function Yishak(){
+  return (
+        <>
+    
+       <div className="yishakl">
+          <header className='yishak2'> 
+            <img src={IMG_3241} className="logo react" alt="React logo" />
+            <h1>Welcome to Yishak's App</h1>
+            <p>This is a simple React application.</p>
+            <h2>Getting Started</h2>
+            <ul>
+               <li>1.Explore the codebase.</li>
+               <li>2.Make changes and see them live.</li>
+               <li>3.Have fun building with React!</li>
+           </ul>
+            <h4>Enjoy your coding journey!</h4>
+
+          </header>
+          </div>
+      </>
+      )
+      }
+      export default Yishak;
